55bb13f2eba8785c8804e02f6f2b951d5d6772cb
Massimiliano Pesente <pesente.m@gmail.com>
2017-07-25 11:42:29 +0200

    Add wallet resource

---
+++ src/Soldo/Resources/Wallet.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Soldo\Resources;
+
+/**
+ * Class Wallet
+ * @package Soldo\Resources
+ *
+ * @property string id
+ * @property string name
+ * @property string currency_code
+ * @property int available_amount
+ * @property int blocked_amount
+ * @property string primary_user_type
+ * @property string primary_user_public_id
+ * @property string custom_reference_id
+ * @property boolean visible
+ */
+final class Wallet extends SoldoResource
+{
+
+    /**
+     * Define resource URL
+     */
+    const RESOURCE_PATH = '/wallets';
+
+
+
+
+}
