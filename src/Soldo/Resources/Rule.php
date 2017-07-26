c84d747944a4538859316163c4c09c032444569b
Massimiliano Pesente <pesente.m@gmail.com>
2017-07-26 17:52:32 +0200

    Add Rule resource

---
+++ src/Soldo/Resources/Rule.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Soldo\Resources;
+
+/**
+ * Class Rule
+ * @package Soldo\Resources
+ *
+ * @property string name
+ * @property bool enabled
+ * @property float amount
+ */
+final class Rule extends SoldoResource
+{
+
+
+
+}
