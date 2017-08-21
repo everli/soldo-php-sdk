1c2bca31e1f6b5037c4eed3b6b014f37ae499e99
Massimiliano Pesente <pesente.m@gmail.com>
2017-08-21 18:41:58 +0200

    Add exceptions for Events

---
+++ src/Soldo/Exceptions/SoldoInvalidEvent.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Soldo\Exceptions;
+
+/**
+ * Class SoldoInvalidEvent
+ * @package Soldo\Exceptions
+ */
+class SoldoInvalidEvent extends SoldoException
+{
+}
---
+++ src/Soldo/Exceptions/SoldoInvalidFingerprintException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Soldo\Exceptions;
+
+/**
+ * Class SoldoInvalidFingerprintException
+ * @package Soldo\Exceptions
+ */
+class SoldoInvalidFingerprintException extends SoldoException
+{
+}
